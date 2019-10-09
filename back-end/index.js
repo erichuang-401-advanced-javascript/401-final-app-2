f413d1661e09f1b2c9838e1202ede0ebdfc99dbe
Eric Huang <ejhuang84@gmail.com>
2019-10-09 09:54:27 -0700

    backend get post delete up

---
+++ back-end/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const app = require('./src/app').start;
+
+app(4000);
---
+++ back-end/src/app.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const uuid = require('uuid/v4');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+
+/* ========================================================
+ Routes
+======================================================== */
+app.get('/things', getThings);
+app.post('/things', makeThing);
+app.delete('/things/:id', deleteThing);
+
+/* ========================================================
+ Helper Functions / Storage
+======================================================== */
+let things = {
+  'test-id-0': { name: 'test1', thing: 'thing' },
+  'test-id-1': { name: 'test2', thing: 'other thing' },
+};
+
+function getThings(request, response){
+  response.json(things);
+}
+
+function makeThing(request, response){
+  things[uuid()] = request.body;
+  response.json(things);
+}
+
+function deleteThing(request, response){
+  delete things[request.params.id];
+  response.json(things);
+}
+
+
+module.exports = {
+  app,
+  start: (port) => {
+    app.listen(port, () => {
+      console.log(`Up on server: ${port}`);
+    });
+  },
+};
